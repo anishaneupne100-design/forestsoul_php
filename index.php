0640ddf0839d74a03b1719d3a2b37638c3c928cf
anishaneupne100-design <anishaneupane735@gmail.com>
2025-11-28 14:47:15 +0545

    Added tailwind setup

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once 'head.php';
 ?>
 
 
-<body class="bg-background-light dark:bg-background-dark font-display">
+<body class="body">
 <div class="relative flex h-auto min-h-screen w-full flex-col overflow-x-hidden">
 <div class="layout-container flex h-full grow flex-col">
 <div class="px-4 sm:px-10 md:px-20 lg:px-40 flex flex-1 justify-center py-5">
@@ -27,10 +27,10 @@ include_once 'head.php';
 </div>
 <div class="hidden md:flex flex-1 justify-end gap-8">
 <nav class="flex items-center gap-9">
-<a class="text-text-main-light dark:text-text-main-dark text-sm font-medium leading-normal hover:text-primary transition-colors" href="#">Features</a>
-<a class="text-text-main-light dark:text-text-main-dark text-sm font-medium leading-normal hover:text-primary transition-colors" href="#">Donate</a>
-<a class="text-text-main-light dark:text-text-main-dark text-sm font-medium leading-normal hover:text-primary transition-colors" href="#">Community</a>
-<a class="text-text-main-light dark:text-text-main-dark text-sm font-medium leading-normal hover:text-primary transition-colors" href="#">About Us</a>
+<a class="nav-link" href="#">Features</a>
+<a class="nav-link" href="#">Donate</a>
+<a class="nav-link" href="#">Community</a>
+<a class="nav-link" href="#">About Us</a>
 </nav>
 <div class="flex gap-2">
 <button class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-surface-light dark:bg-surface-dark text-text-main-light dark:text-text-main-dark text-sm font-bold leading-normal tracking-[0.015em] hover:bg-opacity-80 transition-colors" onclick="location.href='login.php'">
