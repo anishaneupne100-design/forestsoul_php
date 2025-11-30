ad5aa0f456b665a0daf5becabab52108e02873f6
anishaneupne100-design <anisha.neupane.100@gmail.com>
2025-11-30 09:42:15 +0545

    added index pages

--- yoga/index.php
+++ yoga/index.php
@@ -54,7 +54,6 @@ include '../head.php';
 <!-- ImageGrid -->
 <div class="grid-auto p-4">
 <a href="<?php echo url('yoga_gentle_hatha'); ?>" class="col gap-3 pb-3 hover:opacity-80 transition-opacity">
-<div class="img-portrait" data-alt="Woman doing yoga in a bright, sunlit room" style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuBJ2Vj-rx1Z75iH067FWZ6bHKr-nxhjf-k_BpOV-ju-CfHahpr_YDGKosQ3vt4Son_WJ6gan6qNEApWQlxCdDUCHMdQ-Pz2Y_9uLOK2IKL1NsWXQSz6rlx8qoq6Frwv4SK31dsOIm-hthW9hm-2gLOCgv9A_yRQhwmmOwaIA5I0W9liomN_m1XQRrbBEUSMcBXxn_-GkS46wgfzHmiZDApM6YG6DRZw5n5NLBqxtj8EGEi1UDhB00d5x5aacLUWXbf2wef-ieuPxJs");'></div>
 <div>
 <p class="text-white txt-md">Sunrise Vinyasa Flow</p>
 <p class="text-[#92c9a4] txt-sm">Instructor: Anya Sharma</p>
@@ -62,7 +61,6 @@ include '../head.php';
 </div>
 </a>
 <a href="<?php echo url('yoga_gentle_hatha'); ?>" class="col gap-3 pb-3 hover:opacity-80 transition-opacity">
-<div class="img-portrait" data-alt="Man meditating on a yoga mat outdoors" style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuBTO08YXlFZOweeyO3VJDp7YeJvxVSnjGQASxsAIsVWHLI60l8JyvSNQjXumdypYHGaOPVFxOP77TsGpBg3-VhSpOVE4DztJgf7CBX52_j6gbzR4mJ3Tn4h2Wh10zTMXYazaoxQJCD3tySqEDhdR2kxgWJDFrhm-15eUDyB8WjPjdJJgQfAwAEp_ZQkfaSZqWjXhthwlWs3uvA3Y1doN66Wylf1lFevOjbOzdxLMVrGx7RBn4BhiGsUQ_lQ_-hmzDMeO94dMliIm0E");'></div>
 <div>
 <p class="text-white txt-md">Gentle Hatha &amp; Meditation</p>
 <p class="text-[#92c9a4] txt-sm">Instructor: Leo Chen</p>
@@ -103,28 +101,24 @@ include '../head.php';
 <!-- Instructor Spotlight Section -->
 <div class="grid grid-cols-2 md:grid-cols-4 gap-lg p-4">
 <div class="col items-center text-center gap-3">
-<div class="w-24 h-24 bg-center bg-no-repeat aspect-square bg-cover rounded-full" data-alt="Portrait of Anya Sharma, a yoga instructor" style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuCcCSX80fJuf2uQzhEPpSVX1C33GdQYVmVUtFLFH7263ezDoYH1eqFmX1F1NP5GCVnM1j4rE1OLokyZZ1LExfK7ILDttJGauEi1qWWAXTAkWAqyBZpj43LDalvId94E9R5MjQKYTab_oYqeWozi-mVwaZLRxQ6jQM_16DluHsRFwZ4dOV-e1VlJN_c1mGZ37hT6KwKdjnCpIS8A6CptuySsGcndtYpj8vuxX-1B-E-IoVcoe8RsYDnzJT76PqFvngI-_VlQP8HuYsc");'></div>
 <div>
 <p class="text-white txt-md">Anya Sharma</p>
 <p class="text-[#92c9a4] txt-sm">Vinyasa &amp; Flow</p>
 </div>
 </div>
 <div class="col items-center text-center gap-3">
-<div class="w-24 h-24 bg-center bg-no-repeat aspect-square bg-cover rounded-full" data-alt="Portrait of Leo Chen, a yoga instructor" style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuCDgulsbbmw-iaYSnL40xLm31iu_gCAR6v4zXwft2utqzblwRM0bv4LkQmyXwXmFu9TibxMtAebmKAjxOB32-HjnvNLLo6y8XMiPUVBjqRqHYj5qGmTQ12WQkRQQszlzDRE68TWGuzvdTRzSd3mdvzD6K0Kma8BoWSEsmKq_-o9jz2oJKWj0Qskm714KUw0cEgdVoT1bV1rhI3fCpq1pO_GWWZ9CfepFbT6lf8ww-3jG9bpKMXX_6i05xW9Zu2k5jGuYPvLzFQRwQg");'></div>
 <div>
 <p class="text-white txt-md">Leo Chen</p>
 <p class="text-[#92c9a4] txt-sm">Hatha &amp; Meditation</p>
 </div>
 </div>
 <div class="col items-center text-center gap-3">
-<div class="w-24 h-24 bg-center bg-no-repeat aspect-square bg-cover rounded-full" data-alt="Portrait of Sofia Rossi, a yoga instructor" style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuB0MsBW4636lPqTyE6nd74NU3q_heOv1PshdK8qZXfVzqZldnA3dTRbOsKdB-1ODgT7sSrtR9YgHrrsSRptbmV-zfZfJFYjwSb6lX7vB8tsjWFRoSGxSs5kHDluK6FfloPwnAA4Z7V38dDrWxKdPQ7SujE3rjkklFVrWbzPMWxTWlrkY0wSuzbAPy7ir9gh45An01YycqE3_9G9xHu3XZb2B2-tXjZ9EhhnC5_xfd_GgbxrC-LV5ybSv9IjSvb48CZiNrtSbD5fh0M");'></div>
 <div>
 <p class="text-white txt-md">Sofia Rossi</p>
 <p class="text-[#92c9a4] txt-sm">Yin &amp; Restorative</p>
 </div>
 </div>
 <div class="col items-center text-center gap-3">
-<div class="w-24 h-24 bg-center bg-no-repeat aspect-square bg-cover rounded-full" data-alt="Portrait of Marcus Cole, a yoga instructor" style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuBTMX2TF_9T3o8M0vTWSy4zQlF6eKEq2za4h5pAR7JzMofh3Xg1TZtzabG60UlW93EWb7w2Mcf4rKe_RDQX0ZuTkzVbdqBJWOgfXZdMecYvk-BmxTFexYgyMdn4NCwneMYhd33gbrlYi5KJ8VzNGDzNca8QjZgSkWQGQtnwsYhxVMugdGCmRCV3N11wviff-rLPwXme8fSjQEAhPoqoG3DEFNKaMe4zhz3K7Ep5_e1x0bDpAFKGTFOgJfc9FHLJq0wV81XlW6HI3Bs");'></div>
 <div>
 <p class="text-white txt-md">Marcus Cole</p>
 <p class="text-[#92c9a4] txt-sm">Power &amp; Strength</p>
